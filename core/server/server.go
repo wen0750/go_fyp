dd4df3d24848a61b0ea6241400a948490a662c53
sam19167 <94618870+sam19167@users.noreply.github.com>
2023-04-12 16:42:21 +0800

    Add some comments

--- core/server/server.go
+++ core/server/server.go
@@ -1,3 +1,6 @@
+//For testing:
+//1. Use Postman and type [kali IP]:8888/editor
+
 package main
 
 import (
@@ -7,15 +10,16 @@ import (
 
 func main() {
 	router := gin.Default()
-
+	//Use POST method to receive json data from Website
+	// "/editor" is a temporary URL
 	router.POST("/editor", func(c *gin.Context) {
-		id := c.Query("id")
-		page := c.DefaultQuery("page", "0")
-		name := c.PostForm("name")
-		message := c.PostForm("message")
-		fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+		id := c.Query("id")						//get id from header
+		page := c.DefaultQuery("page", "0")		//get page from header
+		name := c.PostForm("name")				//get key from body
+		message := c.PostForm("message")		//get key from body
+		fmt.Printf("id: %T; page: %T; name: %T; message: %T", id, page, name, message)
 
-		c.JSON(200, gin.H{
+		c.JSON(200, gin.H{ //
 			"message": message,
 			"status":  200,
 		})
