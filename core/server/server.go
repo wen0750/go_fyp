c3ead920581b6d65c40b559cf833419c648bc509
sam19167 <94618870+sam19167@users.noreply.github.com>
2023-04-12 05:07:30 -0400

    Update server.go

--- core/server/server.go
+++ core/server/server.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,14 +14,14 @@ func main() {
 	//Use POST method to receive json data from Website
 	// "/editor" is a temporary URL
 	router.POST("/editor", func(c *gin.Context) {
-		id := c.Query("id")						//get id from header
-		page := c.DefaultQuery("page", "0")		//get page from header
-		name := c.PostForm("name")				//get key from body
-		message := c.PostForm("message")		//get key from body
+		id := c.Query("id")                 //get id from header
+		page := c.DefaultQuery("page", "0") //get page from header
+		name := c.PostForm("name")          //get key from body
+		message := c.PostForm("message")    //get key from body
 		fmt.Printf("id: %T; page: %T; name: %T; message: %T", id, page, name, message)
 
 		//It will show the json formated data below when it success
-		c.JSON(200, gin.H{ 
+		c.JSON(200, gin.H{
 			"message": message,
 			"status":  200,
 		})
