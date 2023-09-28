5dd663f18a89242d7b8bff4f3ca9cbf30fecb988
sam19167 <94618870+sam19167@users.noreply.github.com>
2023-09-28 23:50:07 +0800

    Update editor_template.jsx

--- core/frontend/src/component/editor_template.jsx
+++ core/frontend/src/component/editor_template.jsx
@@ -110,8 +110,8 @@ export default class EditorTemplate extends React.Component {
             },
             {
                 key: 4,
-                label: "cve-id",
-                type: "Autocomplete",
+                label: "remediation",
+                type: "multiline",
                 visible: true,
                 removable: false,
             },
